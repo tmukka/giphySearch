cbbca334c3a367081be08b6091d400691bdd43e3
Tharun Mukka <Tharun.Mukka@mrcooper.com>
2017-11-04 12:59:46 +0530

    giphy search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { Http, Response } from '@angular/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Giphy-Search';
+  link = 'http://api.giphy.com/v1/gifs/search?api_key=dc6zaTOxFJmzC&q=';
+  apiLink = '';
+  giphes = [];
+  http: Http;
+  constructor(http: Http) {
+    this.http = http;
+  }
+  performSearch(searchTerm: HTMLInputElement): void {
+    this.apiLink = this.link + searchTerm.value;
+    this.http.request(this.apiLink).subscribe((res: Response) => {
+      this.giphes = res.json().data;
+      console.log(res.json());
+    });
+  }
 }
